3ed9b07a54886a42bd6c893e4ec1dfdd2be21762
Luis Angel <luisparr14@gmail.com>
2022-04-07 13:17:21 -0500

    Add NavBar

---
+++ components/headers/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+export const NavBar = () => {
+
+  return (
+    <React.Fragment>
+      <nav className="navbar">
+        <div className="navbar-nav">
+          <label className="logo">
+            <a href="#aboutme" className="nav-link">
+              Luis Angel
+            </a>
+          </label>
+          <label id="collapse-button" className="navbar-toggler" type="button">
+            <span></span>
+          </label>
+          <ul className="nav-items">
+            <li><a className="nav-link" href="#aboutme">Sobre mi</a></li>
+            <li><a className="nav-link" href="#skills">Habilidades</a></li>
+            <li><a className="nav-link" href="#projects-desktop">Proyectos</a></li>
+            <li><a className="nav-link" href="#contact">Contactame</a></li>
+          </ul>
+        </div>
+      </nav>
+      <ul id="collapseNav" className="nav-items-collapse">
+        <li><a href="#aboutme">Sobre mi</a></li>
+        <li><a href="#skills">Habilidades</a></li>
+        <li><a href="#projects-desktop">Proyectos</a></li>
+        <li><a href="#contact">Contactame</a></li>
+      </ul>
+    </React.Fragment>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import Script from 'next/script';
 import '../styles/globals.css'
-
+import '../styles/NavBar.css';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity='sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=' crossOrigin='anonymous'/>
+      <Script src='/js/main.js'/>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
